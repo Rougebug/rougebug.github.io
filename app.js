79fbdde97d2fdbb02ba886e00c516d0d56fa29bf
Ouissal Meskine <wissalmky@gmail.com>
2024-09-15 21:24:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
     for(const image of images){
         fetch("https://dog.ceo/api/breeds/image/random")
         .then(response => response.json())
-        .then(data => image.src = data.message)
+        .then(data => {
+            image.src = data.message
+            image.width = 100;
+            image.height = 100;
+
+        })
     }
 
 })
